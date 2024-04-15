704a970284500428464b362e755da0e750a4b4c6
ArthurDev <arthurdevoficial@gmail.com>
2024-04-14 21:09:01 -0300

    robot: Basic setup for robot class

---
+++ robot/include/robot.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <bluetooth.h>
+#include <ultrasonic.h>
+#include <Servo.h>
+#include <motor.h>
+
+class Robot {
+private:
+	Motor* leftMotor;
+	Motor* rightMotor;
+	Servo servo;
+
+	Ultrasonic* ultrasonic;
+
+	Bluetooth* bluetooth;
+
+public:
+	Robot();
+
+	void setupLeftMotor(int enable, int input1, int input2);
+	void setupRightMotor(int enable, int input1, int input2);
+	void setupUltrasonic(int trigger, int echo);
+	void setupBluetooth(int receiver, int transmitter);
+	void setupServo(int pin);
+
+
+
+};
---
+++ robot/src/robot.cpp
@@ -0,0 +1,25 @@
+#include <robot.h>
+
+Robot::Robot() {
+	
+}
+
+void Robot::setupLeftMotor(int enable, int input1, int input2) {
+	leftMotor = new Motor(enable, input1, input2);
+}
+
+void Robot::setupRightMotor(int enable, int input1, int input2) {
+	rightMotor = new Motor(enable, input1, input2);
+}
+
+void Robot::setupUltrasonic(int trigger, int echo) {
+	ultrasonic = new Ultrasonic(trigger, echo);
+}
+
+void Robot::setupBluetooth(int receiver, int transmitter) {
+	bluetooth = new Bluetooth(receiver, transmitter);
+}
+
+void Robot::setupServo(int pin) {
+	servo.attach(pin);
+}
